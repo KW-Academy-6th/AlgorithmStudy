6eda2c01ad31876e02ef0c61d390f2259243e823
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:45:37 +0900

    Update 문자열내의p와y의개수.py

--- Study_1/문자열내의p와y의개수.py
+++ Study_1/문자열내의p와y의개수.py
@@ -1,5 +1,1 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/12916
 
-def solution(s):
-    # 받은 문자열을 모두 소문자로 변환 후 개수를 세서 비교한다.
-    return True if s.lower().count('p') == s.lower().count('y') else False
