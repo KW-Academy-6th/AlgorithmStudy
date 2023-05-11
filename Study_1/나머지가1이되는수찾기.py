6cd1074771d603896a43d6c6b50c81a0b5b93e29
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:41:30 +0900

    Update 나머지가1이되는수찾기.py

--- Study_1/나머지가1이되는수찾기.py
+++ Study_1/나머지가1이되는수찾기.py
@@ -1,1 +1,6 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/87389
 
+def solution(n):
+    for i in range(2, n): #2부터 n-1까지
+        if n % i == 1: #나누었을때 나머지가 1이면
+            return i #나눈 값 반환
