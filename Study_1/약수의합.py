865cf8c2f0db05b59fcc8dceb910bbb563f95369
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:42:58 +0900

    Update 약수의합.py

--- Study_1/약수의합.py
+++ Study_1/약수의합.py
@@ -1,1 +1,10 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12928
 
+def solution(n):
+    answer = 0
+    
+    for i in range(n, 0, -1):
+        if n % i == 0:
+            answer += i
+            
+    return answer
