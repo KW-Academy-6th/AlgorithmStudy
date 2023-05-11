31fc861eb35c22bb6e9ca22dbc84700abf236599
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:19:46 +0900

    Update 폰켓몬.py

--- Study_2/폰켓몬.py
+++ Study_2/폰켓몬.py
@@ -1,1 +1,6 @@
-
+def solution(nums):
+    
+    count = len(nums) #폰켓몬의 마릿수를 구한다.
+    type = len(set(nums)) #폰켓몬의 종류의 개수를 구한다.
+    
+    return count//2 if count//2 < type else type #마릿수/2와 종류의 수 중 큰 작은 쪽이 반환됩니다.
