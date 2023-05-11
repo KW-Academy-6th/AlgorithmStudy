f6d12ad66846a9ff28f24e8adcb6834fda22e012
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:41:00 +0900

    Update x만큼간격이있는n개의숫자.py

--- Study_1/x만큼간격이있는n개의숫자.py
+++ Study_1/x만큼간격이있는n개의숫자.py
@@ -1,1 +1,9 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12954
 
+def solution(x, n):
+    answer = []
+    sumNumber = 0
+    for i in range(0, n):
+        sumNumber += x #0부터 x씩 증가합니다.
+        answer.append(sumNumber) #증가한 숫자를 리스트에 하나씩 입력 합니다.
+    return answer
