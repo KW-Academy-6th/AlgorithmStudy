d3ee72c24711484a861a9eaa690279c0af73b7eb
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-11 21:26:49 +0900

    Update 올바른괄호.py

--- Study_4/올바른괄호.py
+++ Study_4/올바른괄호.py
@@ -1,1 +1,16 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
+def solution(s):
+    stack = []
+    
+    for value in s:
+        if not stack and value == ')': #첫 입력이 )일때 False 반
+            return False
+        elif value == ')': #입력이 )일때 이전 입력 삭제
+            stack.pop()
+        else: #입력이 (일때 스텍에 입
+            stack.append(value)
+    
+    if not stack: #작업이 끝났을때, 스텍이 비어있다면 올바른 괄호
+        return True
+    return False
