7a5ff2b4f2a2e82ffe6e0139333630440a63da6e
myoungjin-kim <127969846+myoungjin-kim@users.noreply.github.com>
2023-05-18 15:10:18 +0900

    Update 주식가격.py

--- Study_5/주식가격.py
+++ Study_5/주식가격.py
@@ -1,1 +1,18 @@
+from collections import deque
 
+def solution(prices):
+    answer = []
+    pricesQueue = deque(prices) #가격정보를 가지도있는 큐
+    lenPrices = len(prices) #주식의 개수
+    
+    #주식을 순차적으로 반복
+    for i in range(lenPrices):
+        count = 0
+        currentQueue = pricesQueue.popleft() #현재 확인할 주식의 가격
+        #현재 확인할 주식의 가격과 이후의 가격과의 순차적 비교
+        for j in range(i+1, lenPrices):
+            count += 1
+            if currentQueue > prices[j]:
+                break
+        answer.append(count)
+    return answer
