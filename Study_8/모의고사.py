f8aa3ecb0e971baf6bf9d194c25d96a45c79e8be
figmarigold115 <128561637+figmarigold115@users.noreply.github.com>
2023-07-20 13:20:03 +0900

    Update 모의고사.py

--- Study_8/모의고사.py
+++ Study_8/모의고사.py
@@ -1,13 +1,21 @@
-def solution(sizes):
-    answer = 0
-    max1, max2 = 0, 0                   # 가로와 세로의 최댓값을 저장할 변수
-    for i in sizes:                     # 가로 길이와 세로 길이 중 더 큰 값을 0번 index로 옮김
-        if i[0] <= i[1]:
-            i[0], i[1] = i[1], i[0]
-    for j in sizes:                     # 가로 길이 중 최대, 세로 길이 중 최대를 계산
-        if max1 <= j[0]:
-            max1 = j[0]
-        if max2 <= j[1]:
-            max2 = j[1]
-    answer = max1 * max2                # 지갑의 크기 계산
+def solution(answers):
+    answer = []
+    cnt1, cnt2, cnt3 = 0, 0, 0              # 점수 계산 변수
+    s1 = [1, 2, 3, 4, 5]                    # 각 학생들의 찍는 방식
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    for i in range(len(answers)):           # 답을 비교해서 점수 계산
+        if answers[i] == s1[i%5]:
+            cnt1 += 1
+        if answers[i] == s2[i%8]:
+            cnt2 += 1
+        if answers[i] == s3[i%10]:
+            cnt3 += 1
+    max_score = max(cnt1, cnt2, cnt3)       # 가장 높은 점수
+    if max_score == cnt1:                   # 가장 높은 점수인 학생을 answer에 append
+        answer.append(1)
+    if max_score == cnt2:
+        answer.append(2)
+    if max_score == cnt3:
+        answer.append(3)
     return answer
