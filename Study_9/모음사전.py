6ccd570aaf792fcfbc3fd8f6038c695910dba157
figmarigold115 <128561637+figmarigold115@users.noreply.github.com>
2023-07-27 13:24:34 +0900

    Update 모음사전.py

--- Study_9/모음사전.py
+++ Study_9/모음사전.py
@@ -3,7 +3,7 @@ from itertools import product
 def solution(word):
     answer = 0
     pr = []
-alpha = ['A', 'E', 'I', 'O', 'U']                       # 단어를 만들 알파벳
+    alpha = ['A', 'E', 'I', 'O', 'U']                       # 단어를 만들 알파벳
     for i in range(1, 6):                               # 조합 만들기
         pr += list(product(alpha, repeat=i))
     word_list = [''.join(map(str, p)) for p in pr]      
